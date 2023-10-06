15ce9c964eb062f6f26551537c65d96a51c9d3f2
fuseplan <apoorvsharma54@gmail.com>
2023-10-06 18:35:44 +0530

    First steps

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,23 @@
-export const schemaTypes = []
+import abouts from "./abouts"
+import testimonials from "./testimonials"
+import works from './works'
+import brands from './brands'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+
+export const schemaTypes = [testimonials,
+    abouts,
+    brands,
+    works,
+    experiences,
+    skills,
+    workExperience,
+    contact
+
+
+
+
+]
